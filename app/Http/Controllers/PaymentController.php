79585fb42baa20f383be800f5c42a1c6a0d20306
cperdomo12cr <cperdomo@arcetec.com.co>
2023-11-21 07:12:05 -0500

    fix

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -72,7 +72,7 @@ class PaymentController extends Controller
     {
 
         // Tu lógica para obtener el ACCESS_TOKEN, por ejemplo, desde la autenticación de tu usuario
-        $accessToken = '4ZGlJVahLoSSiitA3pxoBw0vHzmHX7kVZbm0Gg3D_bfn9vKeVOlkouYpqBdd0viP';
+        $accessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpSWGlvX2FPUjBjbDdoQloxZXVzeiJ9.eyJpc3MiOiJodHRwczovL2F1dGguZHJ1by5jb20vIiwic3ViIjoiSGd2aTFKTkR4Q2NmOE9vUG5oVGQwR3puWXRhT1BKQ3dAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZHJ1by1tZXJjaGFudC1hcGkuY29tIiwiaWF0IjoxNzAwNTY4MDc1LCJleHAiOjE3MDA2NTQ0NzMsImF6cCI6IkhndmkxSk5EeENjZjhPb1BuaFRkMEd6bll0YU9QSkN3Iiwic2NvcGUiOiJyZWFkOnRyYW5zYWN0aW9ucyB3cml0ZTp0cmFuc2FjdGlvbnMgcmVhZDpjb25uZWN0IHdyaXRlOmNvbm5lY3Qgd3JpdGU6cGF5bWVudHMgcmVhZDpwYXltZW50cyByZWFkOmNvbm5lY3QtbGluayB3cml0ZTpjb25uZWN0LWxpbmsgd3JpdGU6YWNjb3VudHMgcmVhZDphY2NvdW50cyByZWFkOmVuZC11c2VycyB3cml0ZTplbmQtdXNlcnMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.RUOG8Z6_0eK1XBehVo1DslTPUMqoJ5vBA0EqiMK6TtIoJjgtiz1Xs53sLWdz2HoqQ__Q1g_AcMPkCoYNSVNELL-U4RKqKOuM9Iex98082rd7-Xw6Yc1n8skt8mzCeozpCOlBIFtLyppumxi86d1idk5sVU8X3K8zrYBTFNW380VtUybnGnnPOV8AVKM9deIRBNtaz_3VkXQEnUnQoXKEc99rDOERp3XHXLkCTbJmPNmhnnqRh8vaZZ81RZiISUaFD6I4XA0lrF07HSN44YP16_ymw2Sbcze2H6SoH7j91Ixa8pW9OE5ZC6_TULrKbmzb7VKnfoOK0mp9V7hW-tdbug';
 
         $response = Http::withHeaders([
             'DRUO-Version' => '2021-11-22',
@@ -84,7 +84,7 @@ class PaymentController extends Controller
             "description" => "Insurance policy renewal - INV12324",
             "statement_descriptor" => "NOVO BILLING* INV12324",
             "auto_send_receipt" => false,
-            "funding_source_id" => "acc_f5f1cd79-5efe-4a21-b516-f586dc9105b7"
+            "funding_source_id" => "acc_8e2964c3-7ba1-4d15-8887-d7bd71533254"
         ]);
 
         if ($response->successful()) {
@@ -98,6 +98,40 @@ class PaymentController extends Controller
             $errorResponse = $response->json();
             // Hacer algo con el error, por ejemplo:
             return response()->json(['error' => $errorResponse], $errorCode);
+        }
+    }
+
+    public function oauthToken()
+    {
+        /* curls */
+        /* curl --location 'https://auth.druo.com/oauth/token' \
+        --header 'Content-Type: application/json'  \
+        --data '{
+            "client_id": "{{client_id}}",
+            "client_secret": "{{client_secret}}",
+            "audience": "https://druo-merchant-api.com",
+            "grant_type": "client_credentials"
+        }' */
+
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+        ])
+            ->post('https://auth.druo.com/oauth/token', [
+                'client_id' => config('services.druo.key'),
+                'client_secret' => config('services.druo.secret'),
+                'audience' => 'https://druo-merchant-api.com',
+                'grant_type' => 'client_credentials',
+            ]);
+
+        if ($response->successful()) {
+            $token = $response['access_token'];
+            // Utiliza el token para acceder a la API de DRUO
+            return $response->json();
+        } else {
+            // Manejar la respuesta de error si la solicitud no fue exitosa
+            $error = $response->json();
+            return $error;
         }
     }
 }
