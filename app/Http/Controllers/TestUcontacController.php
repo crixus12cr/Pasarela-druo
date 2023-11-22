eb3c4ae836c12d0f20b14c4f1df5574212f9d898
Christian Perdomo García <cperdomo@arcetec.com.co>
2023-11-22 16:10:55 -0500

    fix

--- app/Http/Controllers/TestUcontacController.php
+++ app/Http/Controllers/TestUcontacController.php
@@ -33,40 +33,77 @@ class TestUcontacController extends Controller
         }
     }
 
+    // public function envioHsm()
+    // {
+
+    //     try {
+
+    //         $token = 'VXNlckFQSVF1YW46YTgwODRmZDUtNDRiYi00OGQyLTliYjAtYzY5MmMxOWJjZTk3';
+    //         $hsm = 'hsm%3B5e49de34-7eb3-4d38-b017-6c67ec4f5cd5%3Btext%3B123456789';
+        
+    //         $response = Http::withHeaders([
+    //             'authorization' => 'Basic '.$token,
+    //             'Content-Type' => 'application/x-www-form-urlencoded',
+    //         ])->post('https://s3a.ucontactcloud.com/Integra/resources/SMS/SendSMSDialer', [
+    //             'message' => $hsm,
+    //             'destination' => '573125620823',
+    //             'dialerName' => 'WhatsAppDialerQuanAPI',
+    //             'agent' => 'UserAPIQuan',
+    //         ]);
+        
+    //         if ($response->successful()) {
+    //             $responseData = $response->body(); 
+                
+    //             return $responseData;
+    //         } else {
+                
+    //             $errorResponse = $response->json(); 
+                
+    //             return response()->json([
+    //                 'status' => 'Error general',
+    //                 'message' => $errorResponse
+    //             ]);
+    //         }
+    //     } catch (\Exception $e) {
+            
+    //         return 'Excepción: '.$e->getMessage();
+    //     }
+    // }
+
     public function envioHsm()
     {
-
-        try {
-
-            $token = 'VXNlckFQSVF1YW46YTgwODRmZDUtNDRiYi00OGQyLTliYjAtYzY5MmMxOWJjZTk3';
-            $hsm = 'hsm%3B5e49de34-7eb3-4d38-b017-6c67ec4f5cd5%3Btext%3B123456789';
+            $token = 'VXNlckFQSVF1YW46ODYzMDQzYmQtNDE4Zi00ZjM1LWFlM2YtYmRkMGQyMTcxYThi';
+            $hsm = 'hsm;5e49de34-7eb3-4d38-b017-6c67ec4f5cd5;text;123456789';
+            $destination = '573125620823';
         
-            $response = Http::withHeaders([
-                'authorization' => 'Basic '.$token,
-                'Content-Type' => 'application/x-www-form-urlencoded',
-            ])->post('https://s3a.ucontactcloud.com/Integra/resources/SMS/SendSMSDialer', [
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, 'https://s3a.ucontactcloud.com/Integra/resources/SMS/SendSMSDialer');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
                 'message' => $hsm,
-                'destination' => '+573125620823',
+                'destination' => $destination,
                 'dialerName' => 'WhatsAppDialerQuanAPI',
                 'agent' => 'UserAPIQuan',
-            ]);
-        
-            if ($response->successful()) {
-                $responseData = $response->body(); 
-                
-                return $responseData;
-            } else {
-                
-                $errorResponse = $response->json(); 
-                
-                return response()->json([
-                    'status' => 'Error general',
-                    'message' => $errorResponse
-                ]);
-            }
-        } catch (\Exception $e) {
+            ]));
             
-            return 'Excepción: '.$e->getMessage();
-        }
+            $headers = [
+                'authorization: Basic ' . $token,
+                'Content-Type: application/x-www-form-urlencoded',
+            ];
+            
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            
+            $result = curl_exec($ch);
+            
+            if (curl_errno($ch)) {
+                echo 'Error:' . curl_error($ch);
+            } else {
+                $decodedResult = json_decode($result); // Decodificar la respuesta JSON
+                var_dump($decodedResult); // Manejar la respuesta JSON como un objeto PHP
+            }
+            
+            curl_close($ch);
     }
 }
